166902e8834aaad3b07a80d8e242f7d469e76e47
JustJoshua <justjoshua@gmail.com>
2021-11-09 08:28:06 -0500

    adding typedefs to index.js file

--- Develop/server/models/index.js
+++ Develop/server/models/index.js
@@ -1,3 +1,20 @@
+const {ApolloServer, gql}= require("apollo-server");
 const User = require('./User');
+const typeDef= gql`
+type Book{
+    authors [String]
+    description String
+    bookId String
+    image String
+    link String
+    title String
+
+}
+type User{
+    username
+    password
+    email
+    savedBooks [Book]
+}`
 
 module.exports = { User };

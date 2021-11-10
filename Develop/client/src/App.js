f533819c42f3f01ee8bf21b59e0d88d8c3af9f29
JustJoshua <justjoshua@gmail.com>
2021-11-10 12:46:00 -0500

    added imports to app.js file

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,6 +3,36 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+}from "@apollo/client";
+import {setContext} from '@apollo/client/link/context';
+import Auth from './utils/auth';
+
+const HttpLink = createHttpLink ({
+  uri: '/graphql',
+}),
+
+const authLink = setContext((_, {Headers}) => {
+  const token= Auth.getToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorizations: token? `Bearer ${token}` : "",
+
+    }
+  }
+}),
+
+const client = new ApolloClient ({
+  // 'http://localhost3001/graphql',
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (

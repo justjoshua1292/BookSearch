6429856c582659874c0648cf86a98232d66b580e
JustJoshua <justjoshua@gmail.com>
2021-11-11 00:17:13 -0500

    deleted extra folders

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,7 +4,7 @@ import {useMutation} from '@apollo/client';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import {LOGIN_USER} from '../utils/mutations';
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
